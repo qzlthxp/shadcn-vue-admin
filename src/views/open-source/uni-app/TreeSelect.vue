242200792c31d94adf65ce88c1172757ef9cb5de
qzlthxp <cc@cMac-mini.local>
2024-12-16 10:38:55 +0800

    添加新菜单

--- src/views/open-source/uni-app/TreeSelect.vue
+++ src/views/open-source/uni-app/TreeSelect.vue
@@ -1,9 +1,21 @@
 <template>
-  <iframe
-    class="w-full h-full"
-    src="https://ext.dcloud.net.cn/plugin?id=10295"
-    frameborder="0"
-  ></iframe>
+  <div class="relative h-full">
+    <iframe
+      class="w-[calc(100%_-_24rem)] h-full"
+      src="https://ext.dcloud.net.cn/plugin?id=10295"
+      frameborder="0"
+    ></iframe>
+
+    <div
+      class="absolute right-0 top-0 w-96 h-full shadow z-10 bg-primary-foreground p-4 rounded-3xl"
+    >
+      <iframe
+        class="w-full h-full bg-white"
+        src="http://121.40.53.138:88/#/"
+        frameborder="0"
+      ></iframe>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
