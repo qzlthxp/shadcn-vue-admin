23ea974bc7528a6c8041bd75ed3149408d536c9c
qzlthxp <cc@cMac-mini.local>
2024-12-24 16:25:53 +0800

    test

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -3,7 +3,8 @@
     <AppSidebar></AppSidebar>
     <SidebarInset class="overflow-hidden">
       <header
-        class="relative flex items-center pl-2 gap-2 h-12 bg-background border-b shrink-0 transition-[width,height] ease-linear">
+        class="relative flex items-center pl-2 gap-2 h-12 bg-background border-b shrink-0 transition-[width,height] ease-linear"
+      >
         <SidebarTrigger />
         <Separator orientation="vertical" class="h-4" />
         <ScrollArea class="group flex-1 whitespace-nowrap">
@@ -11,9 +12,15 @@
             <TagBar />
             <div class="w-6 h-full"></div>
           </div>
-          <ScrollBar class="opacity-0 group-hover:opacity-100 transition-opacity" orientation="horizontal" forceMount />
+          <ScrollBar
+            class="opacity-0 group-hover:opacity-100 transition-opacity"
+            orientation="horizontal"
+            forceMount
+          />
         </ScrollArea>
-        <div class="absolute top-0 bottom-0 right-0 w-6 bg-gradient-to-r from-background/10 to-background"></div>
+        <div
+          class="absolute top-0 bottom-0 right-0 w-6 bg-gradient-to-r from-background/10 to-background"
+        ></div>
       </header>
       <div class="h-[calc(100vh_-_3rem)] overflow-auto">
         <RouterView v-slot="{ Component }">
@@ -29,13 +36,13 @@
 </template>
 
 <script setup>
-import {
-  SidebarProvider,
-  SidebarTrigger,
-  SidebarInset,
-} from '@/components/ui/sidebar';
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { Separator } from '@/components/ui/separator';
-import AppSidebar from './components/AppSidebar.vue';
-import TagBar from '@/components/tag-bar/Index.vue';
+  import {
+    SidebarProvider,
+    SidebarTrigger,
+    SidebarInset,
+  } from '@/components/ui/sidebar';
+  import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+  import { Separator } from '@/components/ui/separator';
+  import AppSidebar from './components/AppSidebar.vue';
+  import TagBar from '@/components/tag-bar/Index.vue';
 </script>
